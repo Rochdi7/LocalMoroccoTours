d0e4850d61a08f8d44aac0a276bd721ac34679d9
Rochdi Karouali <rochdi.karouali1234@gmail.com>
2025-07-10 02:11:05 +0200

    update model comments post

--- app/Models/PostComment.php
+++ app/Models/PostComment.php
@@ -7,27 +7,31 @@ use Illuminate\Database\Eloquent\Model;
 class PostComment extends Model
 {
     protected $fillable = [
-        'post_id', 'user_id', 'parent_id', 'guest_name', 'guest_email',
-        'comment_title', 'comment_body', 'is_approved'
+        'post_id',
+        'user_id',
+        'parent_id',
+        'guest_name',
+        'guest_email',
+        'comment_title',
+        'comment_body',
+        'rating',
+        'images',
+        'helpful_count',
+        'not_helpful_count',
+        'is_approved',
     ];
+
+    protected $casts = [
+        'images' => 'array',
+    ];
+
+    public function ratings()
+    {
+        return $this->hasMany(PostCommentRating::class);
+    }
 
     public function post()
     {
         return $this->belongsTo(Post::class);
-    }
-
-    public function user()
-    {
-        return $this->belongsTo(User::class)->withDefault();
-    }
-
-    public function replies()
-    {
-        return $this->hasMany(PostComment::class, 'parent_id');
-    }
-
-    public function parent()
-    {
-        return $this->belongsTo(PostComment::class, 'parent_id');
     }
 }
