cf3cd944d77ec76de37610f53dfae7ac6a5b1a84
Rochdi Karouali <rochdi.karouali1234@gmail.com>
2025-07-29 00:59:20 +0100

    add kernel to project

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Http\Controllers\SitemapController;
 
 class Kernel extends ConsoleKernel
 {
@@ -12,7 +13,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            app(SitemapController::class)->generate();
+        })->weekly()->sundays()->at('03:00'); 
     }
 
     /**
@@ -21,7 +24,6 @@ class Kernel extends ConsoleKernel
     protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
-
         require base_path('routes/console.php');
     }
 }
